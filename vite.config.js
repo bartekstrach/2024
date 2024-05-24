073e652f9397107d89f76cb0cb2845b35c272790
Bartosz Strach <bartosz_strach@epam.com>
2024-05-24 23:50:45 +0200

    feat: vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from "@vitejs/plugin-react-swc";
-import { createHtmlPlugin } from 'vite-plugin-html'
 import { resolve } from "path";
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
     server: {
@@ -11,29 +11,34 @@ export default defineConfig({
         react({
             devTarget: "es2022"
         }),
-        createHtmlPlugin({
-            entry: resolve(__dirname, 'public', 'bundle.js'),
-            template: resolve(__dirname, 'public', 'index.html'),
-            inject: {
-                data: {
-                    title: 'index',
-                    injectScript: `<script src="./inject.js"></script>`,
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'assets/*',
+                    dest: 'assets'
                 },
-            }
+            ]
         }),
     ],
     define: {
         'process.env.NODE_ENV': JSON.stringify('production'),
     },
+    root: './',
+    publicDir: 'static',
     build: {
+        outDir: 'public',
         rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'public', 'index.html'),
+            input: '/index.html',
+            output: {
+                assetFileNames: 'assets/[name].[hash][extname]',
+                entryFileNames: 'scripts/[name].[hash].js',
+                chunkFileNames: 'scripts/[name].[hash].js',
             },
         },
     },
     resolve: {
         alias: {
+            '@assets': resolve(__dirname, './assets'),
             '@components': resolve(__dirname, './client/components'),
             '@enums': resolve(__dirname, './client/enums'),
             '@types': resolve(__dirname, './client/types'),
